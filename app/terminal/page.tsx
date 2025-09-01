e1df9f292269798793f6fc10c982727882c75b35
bprimal22 <contactbp22@gmail.com>
2025-09-01 15:29:40 -0500

    new landing

---
+++ app/terminal/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "../components/hero"
+
+export default function TerminalPage() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <Hero />
+    </main>
+  )
+}
+
